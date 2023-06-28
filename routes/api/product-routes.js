520609895851f41027f7ebee7e64d3d8713c675d
Simba <dylan.sthilaire@proton.me>
2023-06-27 18:42:09 -0700

    cleanup

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,15 +42,6 @@ router.post('/', async ({ body }, res) => {
   catch (err) { res.status(500).json(err); }
 });
 
-/* req.body should look like this...
-  {
-    product_name: "Basketball",
-    price: 200.00,
-    stock: 3,
-    tagIds: [1, 2, 3, 4]
-  }
-*/
-
 // update product by its 'id' value
 router.put('/:id', async ({ body, params: { id } }, res) => {
   try {
